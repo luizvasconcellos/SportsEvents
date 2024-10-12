0c8bbf6616b289051285d2665df44671afa90400
Luiz Vasconcellos <>
2024-10-12 20:19:28 +0100

    remove Main.storyboard

--- SportsEvents/SportsEvents/ViewController.swift
+++ SportsEvents/SportsEvents/ViewController.swift
@@ -13,8 +13,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let viewModel = SportsEventsViewModel()
+        view.backgroundColor = .green
     }
-
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 
 }
 
